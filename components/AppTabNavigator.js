eb22e35600f10d1c3f59df869193f273cd2bfc5c
Aathi410 <n4aathi04@gmail.com>
2021-01-16 16:24:48 +0530

    Drawer And Settings Screen3

---
+++ components/AppTabNavigator.js
@@ -0,0 +1,26 @@
+import * as React from "react";
+import {Image} from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import CreateTopicScreen from '../screens/CreateTopicScreen';
+import AllTopicScreen from '../screens/AllTopicScreen';
+
+export const AppTabNavigator = createBottomTabNavigator({
+    AllTopics:{
+        screen:AllTopicScreen,
+        navigationOptions:{
+            tabBarIcon:<Image
+                source = {require('../assets/request-list.png')}
+                style = {{width:20, height:20}}/>,
+            tabBarLabel:'All Topics'
+        }
+    },
+    CreateTopicScreen:{
+        screen:CreateTopicScreen,
+        navigationOptions:{
+            tabBarIcon:<Image
+                source = {require('../assets/request-book.png')}
+                style = {{width:20, height:20}}/>,
+            tabBarLabel:'Create A Topic'
+        }
+    },    
+})
